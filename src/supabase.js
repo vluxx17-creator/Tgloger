0a59caab7d4c4597cab02eb0cbd513be19a59c80
DiMaggio <vluxx17@gmail.com>
2026-04-23 19:35:49 +0300

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://lyobddpccijkpqkbrdwq.supabase.co/Mess' // https://supabase.co
-const supabaseKey = 'sb_publishable_Xhyr775MXFE_PM1tRkQMxA_dQbKOpUd'
+const supabaseUrl = 'https://lyobddpccijkpqkbrdwq.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx5b2JkZHBjY2lqa3Bxa2JyZHdxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY5NDY3MjUsImV4cCI6MjA5MjUyMjcyNX0.BU_rCvzlBcVy8jp22kc6OHJkxGGWOXGsZwEd-x8_6Rg'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
