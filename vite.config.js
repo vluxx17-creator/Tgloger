b27014d13b1406011ffc0b0a40c48ef87d9511b7
DiMaggio <vluxx17@gmail.com>
2026-04-23 19:08:52 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,15 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    // Разрешаем Render показывать твой сайт
+    allowedHosts: ['tgloger.onrender.com'],
+    // Настройки для корректного запуска на портах Render
+    port: 10000,
+    host: true
+  },
+  preview: {
+    // Дублируем разрешение для режима предпросмотра
+    allowedHosts: ['tgloger.onrender.com'],
     port: 10000,
     host: true
   }
